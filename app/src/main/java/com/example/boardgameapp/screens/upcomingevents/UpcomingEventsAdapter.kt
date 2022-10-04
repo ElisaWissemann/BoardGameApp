b25a1a1386d3f87bfa50b5c6b9cfa090502e85af
ElisaWissemann <104485810+ElisaWissemann@users.noreply.github.com>
2022-10-04 20:17:20 +0200

    prepare RecyclerView, create upcoming_events_item_view

---
+++ app/src/main/java/com/example/boardgameapp/screens/upcomingevents/UpcomingEventsAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.boardgameapp.screens.upcomingevents
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+class UpcomingEventsAdapter (
+    private val context: Context?,
+//    private val layout: Int) : RecyclerView.Adapter<UpcomingEventsAdapter.UpcomingEventsViewHolder>() {
+
+        //Initialize Data
+       // private val data = Data
+}
