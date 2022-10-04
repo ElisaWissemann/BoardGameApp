da67410588b9dacf67303d23cc93298c86deaf54
ElisaWissemann <104485810+ElisaWissemann@users.noreply.github.com>
2022-10-04 19:10:58 +0200

    set up dummy data

---
+++ app/src/main/java/com/example/boardgameapp/data/event/EventDataSource.kt
@@ -0,0 +1,27 @@
+package com.example.boardgameapp.data.event
+
+object EventDataSource {
+
+    val events: List<Event> = listOf(
+        Event(
+            1,
+            "Julia",
+            3
+        ),
+        Event(
+            2,
+            "Christian",
+            3
+        ),
+        Event(
+            3,
+            "Anna",
+            4
+        ),
+        Event(
+            4,
+            "Elisa",
+            5
+        )
+    )
+}
