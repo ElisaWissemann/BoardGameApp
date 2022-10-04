4a4ce77b26a9bf7ce4053a19d9ed557421bfe789
ElisaWissemann <104485810+ElisaWissemann@users.noreply.github.com>
2022-10-04 21:44:49 +0200

    UpcomingEvents RecyclerView implementation

--- app/src/main/java/com/example/boardgameapp/data/event/EventDataSource.kt
+++ app/src/main/java/com/example/boardgameapp/data/event/EventDataSource.kt
@@ -1,27 +1,33 @@
 package com.example.boardgameapp.data.event
 
+/**
+ * An object to generate a static list of events*/
 object EventDataSource {
 
     val events: List<Event> = listOf(
         Event(
             1,
             "Julia",
-            3
+            3,
+            2,3
         ),
         Event(
             2,
             "Christian",
-            3
+            3,
+            4,1
         ),
         Event(
             3,
             "Anna",
-            4
+            4,
+            1,1
         ),
         Event(
             4,
             "Elisa",
-            5
+            5,
+            3,1
         )
     )
 }
--- app/src/main/java/com/example/boardgameapp/screens/upcomingevents/UpcomingEventsAdapter.kt
+++ app/src/main/java/com/example/boardgameapp/screens/upcomingevents/UpcomingEventsAdapter.kt
@@ -1,12 +1,42 @@
 package com.example.boardgameapp.screens.upcomingevents
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.boardgameapp.data.event.EventDataSource
+import com.example.boardgameapp.databinding.UpcomingEventsItemViewBinding
 
-class UpcomingEventsAdapter (
-    private val context: Context?,
-//    private val layout: Int) : RecyclerView.Adapter<UpcomingEventsAdapter.UpcomingEventsViewHolder>() {
 
-        //Initialize Data
-       // private val data = Data
+class UpcomingEventsAdapter() : RecyclerView.Adapter<UpcomingEventsAdapter.UpcomingEventsViewHolder>() {
+
+    //Initialize Data
+    private val data = EventDataSource.events
+    private lateinit var binding: UpcomingEventsItemViewBinding
+
+    class UpcomingEventsViewHolder(val binding: UpcomingEventsItemViewBinding):RecyclerView.ViewHolder(binding.root) {
+        val hostView = binding.ueHost
+        val dateView = binding.ueDate
+        val acceptedView = binding.ueAccepted
+        val cancelledView = binding.ueCancelled
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UpcomingEventsViewHolder {
+         binding = UpcomingEventsItemViewBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return UpcomingEventsViewHolder(binding)
+    }
+
+
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: UpcomingEventsViewHolder, position: Int) {
+            val item = data[position]
+        holder.hostView?.text = item.host
+        holder.dateView?.text = item.date.toString()
+        holder.acceptedView?.text = item.accepted.toString()
+        holder.cancelledView?.text = item.cancelled.toString()
+        }
+
 }
