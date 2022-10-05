48234f9b058617f77dc54bd58fc205ba61cc978d
ElisaWissemann <104485810+ElisaWissemann@users.noreply.github.com>
2022-10-05 18:14:55 +0200

    fix Datatypes in event, pass event ID from upcomingEvents to events Screen, autogenerate events screen with passed data

--- app/src/main/java/com/example/boardgameapp/screens/event/EventFragment.kt
+++ app/src/main/java/com/example/boardgameapp/screens/event/EventFragment.kt
@@ -2,12 +2,15 @@ package com.example.boardgameapp.screens.event
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.navArgs
 import com.example.boardgameapp.R
+import com.example.boardgameapp.data.event.EventDataSource
 import com.example.boardgameapp.databinding.FragmentEventBinding
 
 class EventFragment : Fragment() {
@@ -19,17 +22,30 @@ class EventFragment : Fragment() {
     private lateinit var viewModel: EventViewModel
     private lateinit var binding: FragmentEventBinding
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-
         binding = FragmentEventBinding.inflate(inflater, container, false)
-
 
         // return view
         return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //Get Data
+        val data = EventDataSource.events
+        // Get EventID passed from upcomingEvents Destination
+        val args: EventFragmentArgs by navArgs()
+        val event = data.find{it.id == args.eventId}
+
+
+        binding.date.text = event?.date
+        binding.host.text = "Host:${event?.host}"
+
     }
 
     override fun onStart() {
@@ -38,5 +54,7 @@ class EventFragment : Fragment() {
 
     }
 
+    fun getUserData(){
 
+    }
 }
