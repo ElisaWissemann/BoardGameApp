cd01411acc27f067974b814e2d4abc60470626c9
ElisaWissemann <eliwi.dev@gmail.com>
2022-10-09 20:41:12 +0200

    Fixed Toolbar, link to profile screen works

--- app/src/main/java/com/example/boardgameapp/MainActivity.kt
+++ app/src/main/java/com/example/boardgameapp/MainActivity.kt
@@ -3,26 +3,17 @@ package com.example.boardgameapp
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.boardgameapp.databinding.ActivityMainBinding
-const val REQUEST_KEY = "101"
-const val BUNDLE_KEY = "B101"
 
 class MainActivity : AppCompatActivity() {
-
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    private val requestKey = REQUEST_KEY
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,48 +22,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         /*Initialize*/
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
         navController = navHostFragment.findNavController()
-        appBarConfiguration = AppBarConfiguration(navController.graph)
         setSupportActionBar(binding.toolbar)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-
+        setupActionBarWithNavController(navController)
 
         // place view into the activities ViewHirarchy
         setContentView(binding.root)
     }
 
-
-
-
-
-
     /*OptionsMenu implementation
     * documentation https://developer.android.com/develop/ui/views/components/appbar
+    * Tutorial https://www.youtube.com/watch?v=yLOsaR_nDrU&list=PLrnPJCHvNZuCamMFswP597mUF-whXoAA6&index=5
     * */
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
-
-    /*Implement pushing navigation to the BackStack*/
-    fun onBackStackChanged() {
-        shouldDisplayHomeUp()
-    }
-
-    fun shouldDisplayHomeUp() {
-        //Enable Up button only  if there are entries in the back stack
-        val canGoBack = supportFragmentManager.backStackEntryCount > 0
-        supportActionBar!!.setDisplayHomeAsUpEnabled(canGoBack)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //this will trigger the click on the menu item if the id of the destination matches the id in dem menu item in menu_main.xml
+        // if it returns null then call super constructor
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         //This method is called when the up button is pressed. Just the pop back stack.
-        navController.navigateUp(appBarConfiguration)
+        navController.navigateUp()
                 || super.onSupportNavigateUp()
         supportFragmentManager.popBackStack()
         return true
