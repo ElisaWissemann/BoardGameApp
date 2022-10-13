a267dcb412bf742c2fc235a830bdcd5c65d0c890
Fr1tzl <95722028+Fr1tzl@users.noreply.github.com>
2022-10-13 14:16:53 +0200

    profileFragment navigation: add Data from UserDataSource

--- app/src/main/java/com/example/boardgameapp/screens/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/boardgameapp/screens/profile/ProfileFragment.kt
@@ -1,40 +1,60 @@
 package com.example.boardgameapp.screens.profile
 
-import android.annotation.SuppressLint
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.boardgameapp.R
-import com.example.boardgameapp.R.id.textView9
-import com.example.boardgameapp.data.event.Event
-import com.example.boardgameapp.data.event.EventDataSource
 import com.example.boardgameapp.data.user.User
 import com.example.boardgameapp.data.user.UserDataSource
-import com.example.boardgameapp.databinding.FragmentEventBinding
-import com.example.boardgameapp.screens.event.EventFragmentArgs
+import com.example.boardgameapp.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
 
     companion object {
         fun newInstance() = ProfileFragment()
     }
-    private lateinit var binding: FragmentEventBinding
 
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var user: User
     private lateinit var viewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val userData = UserDataSource.users
+        val args: ProfileFragmentArgs by navArgs()
+
+        //Log.i("Chris", args.toString())
+
+        user = userData.find { it.id == args.pUserId }!!
+        _binding!!.usernameText.text = getString(R.string.username, user.name)
+        val name = user.name + " " + user.surname
+        _binding!!.nameText.text = getString(R.string.name, name)
+        _binding!!.adressText.text = getString(R.string.address, user.address)
+        //val upcomingEventDate = users.hosting_events
+        //_binding!!.eventText.text = getString(R.string.upcoming_hosting_event_text, upcomingEventDate)
+        _binding!!.favoriteGameText.text = getString(R.string.favorite_game_text, user.favorite_game)
+        _binding!!.favoriteFoodText.text = getString(R.string.favorite_food_text, user.favorite_food)
+    }
+    //TODO RatingAsHost und UpcomingHostingEvents
+    //TODO ScrollView
+    //TODO EditButton DeleteButton
 
 
 
